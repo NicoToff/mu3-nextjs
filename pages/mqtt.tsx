041448f9685c2bb5a3c4d7f776845b9834a58e6d
NicoToff <toffolo01@hotmail.com>
2022-12-20 19:34:20 +0100

    Now uses an MQTT broker directly from front-end!

--- pages/mqtt.tsx
+++ pages/mqtt.tsx
@@ -1,31 +1,69 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import Chip from "@mui/material/Chip";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
-import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
-import { formatTimeAgo } from "../utils/time-ago-formatter";
+import { DataGrid } from "@mui/x-data-grid";
+import type { GridRowsProp, GridColDef, GridValidRowModel } from "@mui/x-data-grid";
 
-type Message = { [key: string]: string };
+import { connect } from "mqtt";
+import type { MqttClient } from "mqtt";
+
+const mqttDomain = "178.32.223.217";
+const port = 9001;
+const mqttUri = `ws://${mqttDomain}`;
+const options = {
+    username: "groupe2",
+    password: "groupe2",
+    port,
+    keepalive: 60,
+};
 
 export default function Mqtt() {
-    const [rows, setRows] = useState<GridRowsProp>([]);
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
 
     useEffect(() => {
-        const interval = setInterval(() => {
-            fetch("http://localhost:3333/api")
-                .then(res => res.json())
-                .then((data: Message) => setRows(makeRows(data)))
-                .catch(console.error);
-        }, 2000);
-        return () => clearInterval(interval);
-    }, [rows]);
+        const timer = setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
+
+        return () => clearInterval(timer);
+    }, [time]);
+
+    const [rows, setRows] = useState<GridRowsProp>([]);
+
+    const client = useRef<MqttClient>();
+    const [mqttConnected, setMqttConnected] = useState(false);
+
+    useEffect(() => {
+        client.current = connect(mqttUri, options);
+        client.current.on("connect", () => {
+            console.log(`Connected to ${mqttUri}`);
+            setMqttConnected(true);
+        });
+        client.current.subscribe("/groupe2/#");
+        client.current.on("message", (topic, message) => {
+            const tag = topic.split("/").pop(); // Gets the last word of the topic
+            if (tag) {
+                const value = message.toString();
+                const timestamp = Date.now().toString();
+                const row = makeRow(tag, value, timestamp);
+                setRows(prevRows => {
+                    const newRows = [...prevRows];
+                    const existingRow = newRows.find(r => r.id === row.id);
+                    if (existingRow) {
+                        Object.assign(existingRow, row); // Updates an existing row
+                    } else {
+                        newRows.push(row); // Adds new row
+                    }
+                    return newRows;
+                });
+            }
+        });
+    }, []);
 
     const columns: GridColDef[] = [
         { field: "mu", headerName: "MU", type: "number", width: 50 },
-        { field: "zone", headerName: "Zone", type: "number", width: 50 },
         { field: "tag", headerName: "Tag", width: 250 },
         { field: "val", headerName: "Valeur", width: 200 },
         { field: "date", headerName: "Date (ISO)", width: 200 },
-        { field: "time", headerName: "Dernier update", width: 200, sortable: false },
     ];
 
     return (
@@ -33,6 +71,11 @@ export default function Mqtt() {
             <Typography variant="h3" component="h1" gutterBottom>
                 MU3 Tags
             </Typography>
+            {mqttConnected ? (
+                <Chip label="MQTT Connected" color="success" sx={{ mb: 3 }} />
+            ) : (
+                <Chip label="MQTT Disconnected" color="error" sx={{ mb: 3 }} />
+            )}
             <div style={{ height: "95vh", width: "99%" }}>
                 <DataGrid rows={rows} columns={columns} />
             </div>
@@ -40,17 +83,12 @@ export default function Mqtt() {
     );
 }
 
-function makeRows(data: Message): GridRowsProp {
-    return Object.entries(data).map(([tag, valAndTs], id) => {
-        const [val, timestamp] = valAndTs.split(";;");
-        return {
-            id,
-            mu: 3,
-            zone: 5,
-            tag,
-            val,
-            date: new Date(parseInt(timestamp)).toISOString(),
-            time: formatTimeAgo(parseInt(timestamp)),
-        };
-    });
+function makeRow(tag: string, val: string, timestamp: string | number): GridValidRowModel {
+    return {
+        id: tag,
+        mu: 3,
+        tag,
+        val,
+        date: new Date(Number(timestamp)).toISOString(),
+    };
 }
